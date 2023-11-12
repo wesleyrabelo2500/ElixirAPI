77cf63b6d917aa8776ea7e2b76e29c442ddbc3bf
wesleyrabelo2500 <56616009+wesleyrabelo2500@users.noreply.github.com>
2023-11-12 18:03:22 -0300

    byby

--- promania/src/application/constants.js
+++
@@ -1,6 +0,0 @@
-export const ROUTES = {
-    SIGN_UP: '/signup',
-    SIGN_IN: '/signin',
-    BOARDS: '/boards',
-    BOARD: '/boards/:board',
-};
--- promania/src/application/services/index.js
+++
@@ -1,2 +0,0 @@
-export { boardService } from './board';
-export { authService } from './auth';
